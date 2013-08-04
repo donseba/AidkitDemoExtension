6abf413064d5c2fd41d99c698c45491202705c03
Sebastiano Bellinzis <mail@sebastiano.me>
2013-08-04 13:08:41 +0200

    Adding some files to the Demo Package

---
+++ install.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return array(
+
+    'routes' => array(
+        "Route::resource('demo1', 'AidkitDemoController1');",
+        "Route::resource('demo2', 'AidkitDemoController2');"
+    )
+);
